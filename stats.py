617dd49b60585811da8b14d72b2eb8d5ba4f1952
Patrick Murphy <155398587+pjmurphycna@users.noreply.github.com>
2026-03-22 22:35:31 -0230

    Update stats.py

--- stats.py
+++ stats.py
@@ -9,7 +9,8 @@ This example is for use on (Linux) computers that are using CPython with
 Adafruit Blinka to support CircuitPython libraries. CircuitPython does
 not support PIL/pillow (python imaging library)!
 """
-
+import time
+import subprocess
 import board
 import digitalio
 from PIL import Image, ImageDraw, ImageFont
