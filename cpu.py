2c2df1909d3e905d8d2da830eb0f562852e4beec
Patrick Murphy <155398587+pjmurphycna@users.noreply.github.com>
2026-03-22 23:04:18 -0230

    Create cpu.py

---
+++ cpu.py
@@ -0,0 +1,91 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+"""
+This demo will fill the screen with white, draw a black box on top
+and then print Hello World! in the center of the display
+
+This example is for use on (Linux) computers that are using CPython with
+Adafruit Blinka to support CircuitPython libraries. CircuitPython does
+not support PIL/pillow (python imaging library)!
+"""
+import time
+import subprocess
+import board
+import digitalio
+from PIL import Image, ImageDraw, ImageFont
+
+import adafruit_ssd1306
+
+# Define the Reset Pin
+oled_reset = digitalio.DigitalInOut(board.D4)
+
+# Change these
+# to the right size for your display!
+WIDTH = 128
+HEIGHT = 32  # Change to 64 if needed
+BORDER = 5
+
+# Use for I2C.
+i2c = board.I2C()  # uses board.SCL and board.SDA
+# i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
+oled = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, i2c, addr=0x3C, reset=oled_reset)
+
+# Use for SPI
+# spi = board.SPI()
+# oled_cs = digitalio.DigitalInOut(board.D5)
+# oled_dc = digitalio.DigitalInOut(board.D6)
+# oled = adafruit_ssd1306.SSD1306_SPI(WIDTH, HEIGHT, spi, oled_dc, oled_reset, oled_cs)
+
+# Clear display.
+oled.fill(0)
+oled.show()
+
+
+
+# Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
+cmd = "hostname -I | cut -d\' \' -f1"
+IP = subprocess.check_output(cmd, shell = True )
+cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
+CPU = subprocess.check_output(cmd, shell = True )
+cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
+MemUsage = subprocess.check_output(cmd, shell = True )
+cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
+Disk = subprocess.check_output(cmd, shell = True )
+
+
+
+# Create blank image for drawing.
+# Make sure to create image with mode '1' for 1-bit color.
+image = Image.new("1", (oled.width, oled.height))
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a white background
+draw.rectangle((0, 0, oled.width, oled.height), outline=255, fill=255)
+
+# Draw a smaller inner rectangle
+draw.rectangle(
+    (BORDER, BORDER, oled.width - BORDER - 1, oled.height - BORDER - 1),
+    outline=0,
+    fill=0,
+)
+
+# Load default font.
+font = ImageFont.load_default()
+
+# Draw Some Text
+text = "CPU: " + str(CPU)
+bbox = font.getbbox(text)
+(font_width, font_height) = bbox[2] - bbox[0], bbox[3] - bbox[1]
+draw.text(
+    (oled.width // 2 - font_width // 2, oled.height // 2 - font_height // 2),
+    text,
+    font=font,
+    fill=255,
+)
+
+# Display image
+oled.image(image)
+oled.show()
